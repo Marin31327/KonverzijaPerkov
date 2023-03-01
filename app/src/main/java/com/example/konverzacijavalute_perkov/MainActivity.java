f34b1b3eade1ff1536e8a01898bf3b4c5cc8d1ce
Marin31327 <marin.perkov2005@gmail.com>
2023-03-01 11:13:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/konverzacijavalute_perkov/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.konverzacijavalute_perkov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+    Spinner izbornik;
+    EditText Unos;
+    TextView USD, EUR, CHF;
+    Button gumb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        izbornik=findViewById(R.id.Izbornik);
+        Unos=findViewById(R.id.UnosIznosa);
+        USD=findViewById(R.id.KolicinaUSD);
+        EUR=findViewById(R.id.KolicinaEUR);
+        CHF=findViewById(R.id.KolicinaCHF);
+        gumb=findViewById(R.id.gumb1);
+        String[] tecaj = {"Srednji", "Kupovni", "Prodajni"};
+        ArrayAdapter ad = new ArrayAdapter<String>(this, com.google.android.material.R.layout.support_simple_spinner_dropdown_item,tecaj);
+        izbornik.setAdapter(ad);
+
+        gumb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Double pareEUR, pareUSD, pareCHF;
+
+                Double amount = Double.parseDouble(Unos.getText().toString());
+
+                if(izbornik.getSelectedItem().toString() == "Srednji"){
+                    pareEUR = (double) Math.round((amount/7.53)*100)/100;
+                    pareUSD = (double) Math.round((amount/7.064)*100)/100;
+                    pareCHF = (double) Math.round((amount/7.65)*100)/100;
+                    EUR.setText(pareEUR.toString());
+                    USD.setText(pareUSD.toString());
+                    CHF.setText(pareCHF.toString());
+                }
+
+                else if (izbornik.getSelectedItem().toString() == "Kupovni"){
+                    pareEUR = (double) Math.round((amount/7.51)*100)/100;
+                    pareUSD = (double) Math.round((amount/7.04)*100)/100;
+                    pareCHF = (double) Math.round((amount/7.63)*100)/100;
+                    EUR.setText(pareEUR.toString());
+                    USD.setText(pareUSD.toString());
+                    CHF.setText(pareCHF.toString());
+                }
+                else if (izbornik.getSelectedItem().toString() == "Prodajni"){
+                    pareEUR = (double) Math.round((amount/7.56)*100)/100;
+                    pareUSD = (double) Math.round((amount/7.08)*100)/100;
+                    pareCHF = (double) Math.round((amount/7.67)*100)/100;
+                    EUR.setText(pareEUR.toString());
+                    USD.setText(pareUSD.toString());
+                    CHF.setText(pareCHF.toString());
+                }
+            }
+        });
+    }
+}
